4abffbc7bb9ec524ec93f480a188e96e6879f4e2
Jeff Teunissen <deek@d2dc.net>
2000-01-01 16:20:38 +0000

    gl_ztrick is now saved in the config.cfg file

--- common/gl_vidlinuxglx.c
+++ common/gl_vidlinuxglx.c
@@ -79,7 +79,7 @@ int		texture_extension_number = 1;
 
 float		gldepthmin, gldepthmax;
 
-cvar_t	gl_ztrick = {"gl_ztrick","1"};
+cvar_t	gl_ztrick = {"gl_ztrick", "1", true};
 
 const char *gl_vendor;
 const char *gl_renderer;
--- common/gl_vidnt.c
+++ common/gl_vidnt.c
@@ -1,23 +1,27 @@
 /*
-Copyright (C) 1996-1997 Id Software, Inc.
+	gl_vidnt.c
+	
+	OpenGL low-level video driver for Windows NT/9x
+	
+	Copyright (C) 1996-1997 Id Software, Inc.
+	Copyright (C) 1999-2000 The QuakeForge Project.
+	Portions Copyright their respective authors.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-// gl_vidnt.c -- NT GL vid component
 
 #include "quakedef.h"
 #include "winquake.h"
@@ -103,8 +107,6 @@ HDC		maindc;
 
 glvert_t glv;
 
-cvar_t	gl_ztrick = {"gl_ztrick","1"};
-
 HWND WINAPI InitializeWindow (HINSTANCE hInstance, int nCmdShow);
 
 viddef_t	vid;				// global video state
@@ -140,18 +142,20 @@ qboolean gl_mtexable = false;
 
 //====================================
 
-cvar_t		vid_mode = {"vid_mode","0", false};
+cvar_t		gl_ztrick = {"gl_ztrick", "1", true};
+
+cvar_t		vid_mode = {"vid_mode", "0", false};
 // Note that 0 is MODE_WINDOWED
-cvar_t		_vid_default_mode = {"_vid_default_mode","0", true};
+cvar_t		_vid_default_mode = {"_vid_default_mode", "0", true};
 // Note that 3 is MODE_FULLSCREEN_DEFAULT
-cvar_t		_vid_default_mode_win = {"_vid_default_mode_win","3", true};
-cvar_t		vid_wait = {"vid_wait","0"};
-cvar_t		vid_nopageflip = {"vid_nopageflip","0", true};
+cvar_t		_vid_default_mode_win = {"_vid_default_mode_win", "3", true};
+cvar_t		vid_wait = {"vid_wait", "0"};
+cvar_t		vid_nopageflip = {"vid_nopageflip", "0", true};
 cvar_t		_vid_wait_override = {"_vid_wait_override", "0", true};
-cvar_t		vid_config_x = {"vid_config_x","800", true};
-cvar_t		vid_config_y = {"vid_config_y","600", true};
-cvar_t		vid_stretch_by_2 = {"vid_stretch_by_2","1", true};
-cvar_t		_windowed_mouse = {"_windowed_mouse","1", true};
+cvar_t		vid_config_x = {"vid_config_x", "800", true};
+cvar_t		vid_config_y = {"vid_config_y", "600", true};
+cvar_t		vid_stretch_by_2 = {"vid_stretch_by_2", "1", true};
+cvar_t		_windowed_mouse = {"_windowed_mouse", "1", true};
 
 int			window_center_x, window_center_y, window_x, window_y, window_width, window_height;
 RECT		window_rect;
