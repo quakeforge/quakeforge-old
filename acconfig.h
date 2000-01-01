233d0af85f7326d08efa94e61e56551df36b8b6d
Joseph Carter <knghtbrd@debian.org>
2000-01-01 20:45:27 +0000

    Added autoheader support.  Also added ./bootstrap to go from a freshly checked out tree to something ./configure'able.

---
+++ acconfig.h
@@ -0,0 +1,17 @@
+/* Version string */
+#define        VERSION         FIXME_DEFINE_THIS!
+
+/* Define if you have the XFree86 DGA extension */
+#undef HAS_DGA
+
+/* Define if you have an old Mesa that defines APIENTRY instead
+   of GLAPIENTRY */
+#undef GLAPIENTRY_IS_APIENTRY
+
+#ifdef GLAPIENTRY_IS_APIENTRY
+#  define GLAPIENTRY   APIENTRY
+#endif
+
+/* Define this if you have GL_COLOR_INDEX8_EXT in GL/gl.h */
+#undef HAVE_GL_COLOR_INDEX8_EXT
+
