c7f95dda2d4a6201f938370af0d8a159f6885d13
Joseph Carter <knghtbrd@debian.org>
2000-01-02 05:00:54 +0000

    Removed _all_ references to the define IDGODS.  It was turned off anyway, but still a glaring security hole waiting for someone to abuse.

--- uquake/net.h
+++ uquake/net.h
@@ -238,10 +238,6 @@ typedef struct
 extern int hostCacheCount;
 extern hostcache_t hostcache[HOSTCACHESIZE];
 
-#ifdef IDGODS
-qboolean IsID(struct qsockaddr *addr);
-#endif
-
 //============================================================================
 //
 // public network functions
