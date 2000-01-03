a841b90d2db8541a35994e2513d27c9d93b781c2
Nelson Rush <palisade@users.sourceforge.net>
2000-01-03 06:18:32 +0000

    SDLQuake merge with our tree made possible by Sam Lantinga and Maas van den Berg, currently the video and cdrom audio are supported.

--- common/cd_wrapper.c
+++ common/cd_wrapper.c
@@ -17,9 +17,12 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-
-#if defined(__linux__)
-	#include "cd_linux.c"
-#else
-	#include "cd_null.c"
+#if defined(SDL)
+	#include "cd_sdl.c"
+#else 
+	#if defined(__linux__)
+		#include "cd_linux.c"
+	#else
+		#include "cd_null.c"
+	#endif
 #endif
