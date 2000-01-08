f2d4e5f06ad54349f61c49457c2cde856037f058
Marcus Sundberg <mackan@stacken.kth.se>
2000-01-08 17:37:08 +0000

    #pragmas are for som Windows compiler. Until we know what compiler we #ifdef them out like this.

--- qw_client/glquake.h
+++ qw_client/glquake.h
@@ -19,9 +19,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // disable data conversion warnings
 
+#if !defined(__GNUC__) && !defined(__unix) && !defined(__unix__)
 #pragma warning(disable : 4244)     // MIPS
 #pragma warning(disable : 4136)     // X86
 #pragma warning(disable : 4051)     // ALPHA
+#endif
 
 #ifdef _WIN32
 #include <windows.h>
