9af36c78ff49c21c215dbf3bd97fb932689814f6
Jeff Teunissen <deek@d2dc.net>
2000-01-11 04:18:13 +0000

    Removed personally offensive copyright notice (mine -- I'll let anyone else take all the credit they want, keep me out of it); edited so that the DGA/Vmode detection functions return 0 if HAS_DGA is not defined.

--- common/dga_check.c
+++ common/dga_check.c
@@ -1,27 +1,29 @@
 /*
-  Routines to check for XFree86 DGA and VidMode extensions
+	dga_check.c
+	
+	Routines to check for XFree86 DGA and VidMode extensions
 
-  Copyright (C) 2000	Jeff Teunissen  [d2deek@pmail.net]
-  Copyright (C) 2000	Marcus Sundberg [mackan@stacken.kth.se]
-  Please see the file "AUTHORS" for a list of contributors
+	Copyright (C) 2000 Contributors of the QuakeForge Project
+	Copyright (C) 2000 Marcus Sundberg [mackan@stacken.kth.se]
+	Please see the file "AUTHORS" for a list of contributors
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 
-  See the GNU General Public License for more details.
+	See the GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to:
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
 
-  Free Software Foundation, Inc.
-  59 Temple Place - Suite 330
-  Boston, MA  02111-1307, USA.
+	Free Software Foundation, Inc.
+	59 Temple Place - Suite 330
+	Boston, MA  02111-1307, USA.
 */
 
 #include <quakedef.h>
@@ -33,6 +35,7 @@
 #ifdef HAS_DGA
 #include <X11/extensions/xf86dga.h>
 #include <X11/extensions/xf86vmode.h>
+#endif
 
 /*
   VID_CheckDGA
@@ -42,6 +45,7 @@
 int
 VID_CheckDGA(Display *dpy, int *maj_ver, int *min_ver)
 {
+#ifdef HAS_DGA
 	int event_base, error_base;
 	
 	if (! XF86DGAQueryExtension(dpy, &event_base, &error_base)) {
@@ -49,6 +53,9 @@ VID_CheckDGA(Display *dpy, int *maj_ver, int *min_ver)
 	}
 
 	return XF86DGAQueryVersion(dpy, maj_ver, min_ver);
+#else
+	return 0;
+#endif	// HAS_DGA
 }
 
 
@@ -60,6 +67,7 @@ VID_CheckDGA(Display *dpy, int *maj_ver, int *min_ver)
 int
 VID_CheckVMode(Display *dpy, int *maj_ver, int *min_ver)
 {
+#ifdef HAS_DGA
 	int event_base, error_base;
 	
 	if (! XF86VidModeQueryExtension(dpy, &event_base, &error_base)) {
@@ -67,6 +75,7 @@ VID_CheckVMode(Display *dpy, int *maj_ver, int *min_ver)
 	}
 
 	return XF86VidModeQueryVersion(dpy, maj_ver, min_ver);
+#else
+	return 0;
+#endif	// HAS_DGA
 }
-
-#endif /* HAS_DGA */
--- common/dga_check.h
+++ common/dga_check.h
@@ -1,29 +1,29 @@
 /*
-  dga_check.h
+	dga_check.h
   
-  Definitions for XFree86 DGA and VidMode support
+	Definitions for XFree86 DGA and VidMode support
   
-  Copyright (C) 2000	Jeff Teunissen  [d2deek@pmail.net]
-  Copyright (C) 2000	Marcus Sundberg [mackan@stacken.kth.se]
-  Please see the file "AUTHORS" for a list of contributors
+	Copyright (C) 2000 Contributors of the QuakeForge Project
+	Copyright (C) 2000 Marcus Sundberg [mackan@stacken.kth.se]
+	Please see the file "AUTHORS" for a list of contributors
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 
-  See the GNU General Public License for more details.
+	See the GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to:
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
   
-  Free Software Foundation, Inc.
-  59 Temple Place - Suite 330
-  Boston, MA  02111-1307, USA.
+	Free Software Foundation, Inc.
+	59 Temple Place - Suite 330
+	Boston, MA  02111-1307, USA.
 */
 
 #ifndef DGA_CHECK_H
