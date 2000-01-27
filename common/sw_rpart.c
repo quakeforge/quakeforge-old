fdec8fa33ae6e35a08c08a0cd90a6b5e7eee9e43
Zephaniah E. Hull <warp@babylon.d2dc.net>
2000-01-27 19:10:22 +0000

    Quick fixup for compiling without the gl headers..

--- common/sw_rpart.c
+++ common/sw_rpart.c
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "r_local.h"
 
 #include "d_iface.h"
-#include "glquake.h"
 #include <mathlib.h>
 
 extern particle_t	*active_particles, *free_particles;
