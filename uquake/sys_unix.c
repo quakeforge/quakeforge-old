6403d62a283d21fdf955c30d7995903025ccdce0
Marcus Sundberg <mackan@stacken.kth.se>
2000-02-05 16:02:39 +0000

    Include qargs.h

--- uquake/sys_unix.c
+++ uquake/sys_unix.c
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 #include "sys.h"
+#include "qargs.h"
+
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
