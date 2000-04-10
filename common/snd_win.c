a29be25f751cc744abc3709440c71e5ad2d5de3e
Anton E. Gavrilov <tonik@vipmail.ru>
2000-04-10 20:33:19 +0000

    Disabled the popup warning window ("sound is in use by another app", retry/cancel).

--- common/snd_win.c
+++ common/snd_win.c
@@ -263,16 +263,17 @@ sndinitstat SNDDMA_InitDirect (void)
 			return SIS_FAILURE;
 		}
 
-		if (MessageBox (NULL,
+/*		if (MessageBox (NULL,
 						"The sound hardware is in use by another app.\n\n"
 					    "Select Retry to try to start sound again or Cancel to run Quake with no sound.",
 						"Sound not available",
 						MB_RETRYCANCEL | MB_SETFOREGROUND | MB_ICONEXCLAMATION) != IDRETRY)
 		{
+*/
 			Con_SafePrintf ("DirectSoundCreate failure\n"
 							"  hardware already in use\n");
 			return SIS_NOTAVAIL;
-		}
+//		}
 	}
 
 	dscaps.dwSize = sizeof(dscaps);
