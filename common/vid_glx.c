5f123902098b74c39b6f3a87292c43439aa1f3ad
Bill Currie <bill@taniwha.org>
2000-05-02 12:01:55 +0000

    remove an unwanted  && vid_fullscreen->value

--- common/vid_glx.c
+++ common/vid_glx.c
@@ -481,7 +481,7 @@ void VID_Init(unsigned char *palette)
 #endif
 #ifdef HAS_VIDMODE
 	hasvidmode = VID_CheckVMode(x_disp, NULL, NULL);
-	if (hasvidmode && vid_fullscreen->value) {
+	if (hasvidmode) {
 		if (! XF86VidModeGetAllModeLines(x_disp, DefaultScreen(x_disp),
 						 &nummodes, &vidmodes)
 		    || nummodes <= 0) {
