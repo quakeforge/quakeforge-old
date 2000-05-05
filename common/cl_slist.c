93c098eb8794330e7ebd13114261271107afc497
Joseph Carter <knghtbrd@debian.org>
2000-05-05 01:55:55 +0000

    Put standard GPL thingy on these things

--- common/cl_slist.c
+++ common/cl_slist.c
@@ -1,3 +1,62 @@
+/*
+	cl_slist.c
+
+	serverlist addressbook
+
+	Copyright (C) 1999,2000  contributors of the QuakeForge project
+	Please see the file "AUTHORS" for a list of contributors
+
+	Author: Brain Koropoff
+	Date: 03 May 2000
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+	$Id$
+*/
+
+#include <cvar.h>
+#include <quakefs.h>
+#include <quakedef.h>
+
+cvar_t	*registered;
+
+/*
+	register_check
+
+	Look for gfx/pop.lmp (only found in ID's registered quake pak files),
+	and set the "registered" cvar to 1 if found.
+*/
+void
+register_check ( void )
+{
+	QFile *h;
+
+	registered = Cvar_Get ("registered","0",0,"None");
+
+	COM_FOpenFile("gfx/pop.lmp", &h);
+
+	if (h) {
+		Cvar_Set (registered, "1");
+		Qclose (h);
+	}
+}
+
 #include <stdlib.h>
 #include <cl_slist.h>
 #include <stdio.h>
